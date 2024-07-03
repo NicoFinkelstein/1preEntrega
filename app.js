f6ead3b920f6761e15f96a840f06f44b4a5237f6
NicoFinkelstein <finkelsteinunl@gmail.com>
2024-07-03 20:02:46 -0300

    Clase1

---
+++ app.js
@@ -0,0 +1,13 @@
+let nombre = "SV DIABLO";
+let matricula = "9503871";
+
+console.log (nombre);
+console.log (matricula);
+
+const buque = {
+    nombre: "SV DIABLO",
+    year: 1,
+    ladrar: () =>{
+        return "OK";
+    }
+}
